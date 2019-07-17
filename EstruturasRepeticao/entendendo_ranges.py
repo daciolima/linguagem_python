78904b4e3b57eb7ab64702cb8e3be754ff94b9fa
daciolima <contato.dacio@gmail.com>
2019-07-17 00:11:50 -0300

    Laco Range

--- EstruturasRepeticao/entendendo_ranges.py
+++ EstruturasRepeticao/entendendo_ranges.py
@@ -7,7 +7,18 @@ Ranges
 Ranges são utilizados para gerar sequencias numéricas, não
 """
 
-# Forma 1
-num = 0
+# Forma 1 - Inicio não especificado, padrão 0.
 for num in range(11):
+    print(num)
+
+# Forma 2 - Inicio especificado pelo usuário
+for num in range(0, 11):
+    print(num)
+
+# Forma 3 - Valor de inicio, valor final, e o salto a cada laço
+for num in range(0, 10, 2):
+    print(num)
+
+# Forma 4 - Inversa - Valor de inicio(final), valor final(Início), e o salto a cada laço - Decrementando
+for num in range(10, 0, -1):
     print(num)
