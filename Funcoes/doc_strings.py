c696f56ab6b6f5b4ab2028b8e081cb86fbf077de
daciolima <contato.dacio@gmail.com>
2019-07-29 01:25:43 -0300

    Exercicios - Docstring

---
+++ Funcoes/doc_strings.py
@@ -0,0 +1,15 @@
+"""
+Documentando função com o Docstring
+
+OBS: Podemos ter acesso a documentação de uma função em python com o comando: __doc__
+ex: print.__doc__
+
+Podemos ainda fazer acesso a documentoção com a função help(funcao())
+
+"""
+
+# Exemplo
+def diz_oi():
+    """Essa é uma função que retorna um OI"""  # Essa anotação é uma docstring
+    return 'Oi'
+
